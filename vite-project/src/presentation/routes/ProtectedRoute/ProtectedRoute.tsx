40642712d093e8445f3792436ad10703cdb09b7d
Cristian Pilon <cpilon@11solutions.cl>
2024-07-01 21:30:30 +0200

    fix:test-credential

--- vite-project/src/presentation/routes/ProtectedRoute/ProtectedRoute.tsx
+++ vite-project/src/presentation/routes/ProtectedRoute/ProtectedRoute.tsx
@@ -12,7 +12,7 @@ export const ProtectedRoute = () => {
       onIdTokenChanged(auth, async (user) => {
         if (!user) {
           logOut();
-          console.log("El usuario ha sido deslogueado - closeSession");
+          console.log("El usuario ha sido deslogueado - closeeSession");
         } else {
           try {
             const token = await user.getIdToken();
